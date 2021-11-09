eaff675a70e51a67e9de8e79f41121ba19ddef8d
lesley <lesleybanadzem2017@gmail.com>
2021-11-09 15:32:14 -0800

    1. Added a post method to the HomePageView to hanlde post request. 2. Created a model class to handle the convertion and downloading downloading of images.

--- imageConverter/app/models.py
+++ imageConverter/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Model:
+
+    def downloadImage():
+        pass
+
+    def convertImageTo():
+        ''''''
+        pass
--- imageConverter/app/views.py
+++ imageConverter/app/views.py
@@ -1,8 +1,16 @@
+from django.http import request
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
+
+from .models import Model
 
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+    def post(self, request, **kwargs):
+        if request.method == "POST":
+            pass
+        return render(request, 'index.html')
