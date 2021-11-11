b4fa17c42951699df3105e4d99e905a2d7f4d395
lesley <lesleybanadzem2017@gmail.com>
2021-11-10 18:44:39 -0800

    Converting Image from heic to any format in the select options.

--- imageConverter/app/models.py
+++ imageConverter/app/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
+from wand.image import Image
 # Create your models here.
 
 
 class Model:
 
-    def downloadImage():
+    def downloadImage(self):
         pass
 
-    def convertImageTo():
-        ''''''
-        pass
+    def convertImageTo(image_file, image_type):
+
+        # img = Image(filename=imagedata)
+        if image_type is None or len(image_type) == 0:
+            image_type = 'jpeg'
+        with Image(blob=image_file) as i:
+            # i.save(filename="/Users/lesley/Desktop/formatImage/" +
+            #        imagename + filetype)
+
+            i.format = image_type
+            return i
--- imageConverter/app/views.py
+++ imageConverter/app/views.py
@@ -17,7 +17,14 @@ class HomePageView(TemplateView):
             image_file = UploadFileForm(data=request.POST, files=request.FILES)
             image_type = ImageFileType(request.POST)
 
-            # if image_file.is_valid and image_type.is_valid():
-            #     Model.convertImageTo()
+            # If
+            if image_file.is_valid() and image_type.is_valid():
+                image_file = request.FILES['image_file']
+                image_type = request.POST['image_type']
+                image_filename = image_file.name[:-4] + image_type
+                converted_image = Model.convertImageTo(image_file, image_type)
+                return render(request, 'index.html', {
+                    "filename": image_filename})
+                print(converted_image, image_filename)
 
         return render(request, 'index.html')
